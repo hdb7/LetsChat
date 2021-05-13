a7f8e6db8b2ec9d02ea8fa7796e285b1f314f780
Hamjak <hamjakdb@yahoo.com>
2021-05-13 19:30:28 +0530

    add socket.io

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 //libraries
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const s_io = require("socket.io");
 //global variable 
-const host = "localhost"
-const port = 3000
-
+const HOST = "localhost";
+const PORT = 3000 || process.env.PORT;
+const server = http.Server(app);
+const io = s_io(server);  //socket.io instance
 // express middleware
 app.use(express.static('public'));
 app.get("/", (req, res) => {
   //res.sendFile(__dirname + "/" + "index.html")
   res.sendFile(path.join(__dirname + "/public/index.html"));
 })
-
-app.listen(port,host, () => {
-  console.log("[Let's Chat]--|listening at| --> http://" + host + ":" + port)
+io.on();
+server.listen(PORT,HOST, () => {
+  console.log("[Let's Chat]--|listening at| --> http://" + HOST + ":" + PORT)
 })
 
