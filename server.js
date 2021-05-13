2929c821d628f97e131f70d7bdc40503cc40bc0f
Hamjak <hamjakdb@yahoo.com>
2021-05-13 21:04:15 +0530

    update !!

--- server.js
+++ server.js
@@ -9,13 +9,29 @@ const HOST = "localhost";
 const PORT = 3000 || process.env.PORT;
 const server = http.Server(app);
 const io = s_io(server);  //socket.io instance
+const users={};
 // express middleware
 app.use(express.static('public'));
 app.get("/", (req, res) => {
   //res.sendFile(__dirname + "/" + "index.html")
   res.sendFile(path.join(__dirname + "/public/index.html"));
 })
-io.on();
+io.on("connection", (socket)=>{
+  socket.on("new-user", (username)=>{
+    users[socket.id]=username;
+    socket.broadcast.emit("user-connected", username);
+  });
+  //receive msg from client
+  socket.on("send-chat-msg", (msg)=>{
+    //console.log(msg);
+    //broadcast to all connected users
+    socket.broadcast.emit("chat-msg", {msg:msg, username:users[socket.id]});
+  });
+  socket.on("disconnect", ()=>{
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
 server.listen(PORT,HOST, () => {
   console.log("[Let's Chat]--|listening at| --> http://" + HOST + ":" + PORT)
 })
